cc6f6939969c89a6bbf6637f4c1e5119782d9f82
Muit-Sowrav <edesh.devops2@gmail.com>
2022-09-20 11:00:04 +0600

    changed profile name

--- resources/views/layouts/admin_layouts.blade.php
+++ resources/views/layouts/admin_layouts.blade.php
@@ -550,7 +550,7 @@
                      class="rounded-circle avatar-md">
                 <div class="dropdown">
                     <a href="javascript: void(0);" class="text-dark font-weight-normal dropdown-toggle h5 mt-2 mb-1 d-block"
-                       data-toggle="dropdown">Stanley Parker</a>
+                       data-toggle="dropdown">Wasim Akram</a>
                     <div class="dropdown-menu user-pro-dropdown">
 
                         <!-- item-->
